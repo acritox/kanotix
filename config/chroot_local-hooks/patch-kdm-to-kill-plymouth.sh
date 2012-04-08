586f37afdf754d607dbba717749387f68d4b16d2
Maximilian Gerhard <maxbox@directbox.com>
2012-04-08 16:42:49 +0200

    moved kdm patch to hook script to check kdm exists
    For builds with other login managers than kdm the build always
    fails because patching /etc/init.d/kdm abort with error. I wrapped
    the patch inside a hook script that ensures /etc/init.d/kdm exists
    before patching.

--- config/chroot_local-hooks/patch-kdm-to-kill-plymouth.sh
+++ config/chroot_local-hooks/patch-kdm-to-kill-plymouth.sh
@@ -1,4 +1,7 @@
---- /build/kanotix/chroot/etc/init.d/kdm	2011-02-12 20:16:11.000000000 +0100
+#!/bin/sh
+if [ -f /etc/init.d/kdm ]; then
+ patch --no-backup-if-mismatch -p0 <<'EOT'
+--- /etc/init.d/kdm	2011-02-12 20:16:11.000000000 +0100
 +++ /etc/init.d/kdm	2011-09-28 04:05:22.977140278 +0200
 @@ -126,6 +126,10 @@
         [ "$(cat $DEFAULT_DISPLAY_MANAGER_FILE)" != "$DAEMON" ]; then
@@ -11,3 +14,7 @@
        log_daemon_msg "Starting K Display Manager" "kdm"
        if start-stop-daemon --start --quiet $SSD_ARGS -- $ARG ; then
          log_end_msg 0
+EOT
+else
+ exit 0
+fi
