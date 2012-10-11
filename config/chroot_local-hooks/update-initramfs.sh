80cacfe57ab1a7e2be7af295ece081ca6fff94c6
Andreas Loibl <andreas@andreas-loibl.de>
2012-10-11 22:31:26 +0200

    various smaller changes
    * removed wine-ppa-oneiric
    * added hack to generate initrd only once
    * added multiarch support for wheezy amd64
    * updated packagelists for multiarch (wine, skype)
    * removed obsolete packagelists includes

--- config/chroot_local-hooks/update-initramfs.sh
+++ config/chroot_local-hooks/update-initramfs.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
+# Disable the diversion that prevented update-initramfs from being run
+# This way we save time by not calling update-initramfs several times
+rm -f /usr/local/sbin/update-initramfs
 
-# This is a hook for live-helper(7) to rebuild the initramfs image.
-# To enable it, copy or symlink this hook into your config/chroot_local-hooks
-# directory.
-#
-# Note: You only want to use this hook if you have modified any initramfs-script
-# during the build and need to refresh the initrd.img for that purpose.
+# Create stubs that will be updated by chroot_hacks afterwards
+touch $(ls /boot/vmlinuz-* | sed 's@^.*/vmlinuz-@/var/lib/initramfs-tools/@g')
 
-for KERNEL in /boot/vmlinuz-*
-do
-	VERSION="$(basename ${KERNEL} | sed -e 's|vmlinuz-||')"
-
-	update-initramfs -k ${VERSION} -t -u
-done
