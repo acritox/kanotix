97685e70eb99edaea1a9a420460ef05fd2b19dcd
Andreas Loibl <andreas@andreas-loibl.de>
2013-08-25 10:11:16 +0200

    further auto hook fixes for live-build 3.0.5

--- auto/live-build/functions/00_init.sh
+++ auto/live-build/functions/00_init.sh
@@ -7,3 +7,11 @@ do
         . "${_FILE}"
     fi
 done
+Set_defaults() {
+    # hide this script from the real Set_defaults
+    # to make sure that no path is set to this local folder
+    mv local local.tmp
+    . /usr/share/live/build/functions/defaults.sh
+    Set_defaults "@"
+    mv local.tmp local
+}
