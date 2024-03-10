81fa68433572d0d2a5f64aa6f0831c0fd6b0814d
Holger Paradies <retabell@gmx.de>
2024-03-10 15:52:14 +0100

    Buildoptions for bookworm slowfire in nightly-build bash

--- nightly_build.bash
+++ nightly_build.bash
@@ -6,7 +6,7 @@ function check_iso {
 
 echo "K: Kanotix start iso check..." >>binary.log
     IERR=0
-if grep -F -q "E: " binary.log; then
+if grep -q "^E: " binary.log; then
     echo "K: build ERROR found...1" >>binary.log
     IERR=1
 fi
@@ -62,6 +62,7 @@ mkdir -p $target $next
 ## remove gfxdetect entries from grub.cfg (for non-gfxdetect-builds)
 #sed -i '/gfxdetect/,/^}/{d}' config/binary_grub/grub.cfg
 
+BUILD_BOOKWORM=false
 BUILD_BULLSEYE=true
 BUILD_BUSTER=true
 BUILD_BUSTER_EXTRA_KDE=true
@@ -70,6 +71,144 @@ BUILD_STRETCH=false
 BUILD_JESSIE=false
 BUILD_JESSIE_SPECIAL=false
 BUILD_WHEEZY=false
+
+################### SLOWFIRE  ###################
+
+if $BUILD_BOOKWORM; then
+git checkout auto/config
+DISTRO=bookworm
+KDISTRO=slowfire
+rm -rf cache tmpfs/cache
+sed -i 's/\(export LB_DISTRIBUTION=\).*/\1"'$DISTRO'"/' auto/config
+
+# kde 64
+lb clean
+lb config -d $DISTRO -p "debpool kanotix-kde-master firefox wine-staging" --bootloader grub2 --tmpfs true --tmpfs-options size=16G --apt-http-proxy "http://127.0.0.1:3142" --cache-packages false --gfxoverlays false -a amd64 --initsystem systemd
+echo Kanotix slowfire-nightly Slowfire64 $d$v KDE > config/chroot_local-includes/etc/kanotix-version
+lb build; cd tmpfs; ./isohybrid-acritox kanotix64.iso
+check_iso
+#ls -hgo kanotix64.iso >>binary.log; rm -f kanotix64.iso
+mv kanotix64.iso $target/kanotix64-slowfire-nightly-${d}${v}-KDE.iso; cd ..
+cp tmpfs/binary.packages $target/kanotix64-slowfire-nightly-${d}${v}-KDE.packages
+cp tmpfs/binary.log $target/kanotix64-slowfire-nightly-${d}${v}-KDE.log
+cd $target
+if [ ! -f kanotix64-slowfire-nightly-${d}${v}-KDE.iso ]; then
+        echo "$KDISTRO amd64 KDE failed" >>isos-failed.txt
+        cd -
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-KDE.log $next/kanotix64-slowfire-nightly-KDE.log
+else
+        md5sum -b kanotix64-slowfire-nightly-${d}${v}-KDE.iso > kanotix64-slowfire-nightly-${d}${v}-KDE.iso.md5
+        zsyncmake kanotix64-slowfire-nightly-${d}${v}-KDE.iso
+        cd -
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-KDE.packages $next/kanotix64-slowfire-nightly-KDE.packages
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-KDE.log $next/kanotix64-slowfire-nightly-KDE.log
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-KDE.iso $next/kanotix64-slowfire-nightly-KDE.iso
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-KDE.iso.zsync $next/kanotix64-slowfire-nightly-KDE.iso.zsync
+        sed "s/${d}${v}-//" $target/kanotix64-slowfire-nightly-${d}${v}-KDE.iso.md5 > $next/kanotix64-slowfire-nightly-KDE.iso.md5
+fi
+#
+# lxde 64
+lb clean
+lb config -d $DISTRO -p "debpool kanotix-lxde-master firefox wine-staging" --bootloader grub2 --tmpfs true --tmpfs-options size=16G --apt-http-proxy "http://127.0.0.1:3142" --cache-packages false --gfxoverlays false -a amd64 --initsystem systemd
+echo Kanotix slowfire-nightly Slowfire64 $d$v LXDE > config/chroot_local-includes/etc/kanotix-version
+lb build; cd tmpfs; ./isohybrid-acritox kanotix64.iso
+check_iso
+#ls -hgo kanotix64.iso >>binary.log; rm -f kanotix64.iso
+mv kanotix64.iso $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.iso; cd ..
+cp tmpfs/binary.packages $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.packages
+cp tmpfs/binary.log $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.log
+cd $target
+if [ ! -f kanotix64-slowfire-nightly-${d}${v}-LXDE.iso ]; then
+        echo "$KDISTRO amd64 LXDE failed" >>isos-failed.txt
+        cd -
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.log $next/kanotix64-slowfire-nightly-LXDE.log
+else
+        md5sum -b kanotix64-slowfire-nightly-${d}${v}-LXDE.iso > kanotix64-slowfire-nightly-${d}${v}-LXDE.iso.md5
+        zsyncmake kanotix64-slowfire-nightly-${d}${v}-LXDE.iso
+        cd -
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.packages $next/kanotix64-slowfire-nightly-LXDE.packages
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.log $next/kanotix64-slowfire-nightly-LXDE.log
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.iso $next/kanotix64-slowfire-nightly-LXDE.iso
+        ln -s $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.iso.zsync $next/kanotix64-slowfire-nightly-LXDE.iso.zsync
+        sed "s/${d}${v}-//" $target/kanotix64-slowfire-nightly-${d}${v}-LXDE.iso.md5 > $next/kanotix64-slowfire-nightly-LXDE.iso.md5
+fi
+
+# kde 32
+lb clean
+lb config -d $DISTRO -p "kanotix-kde-master firefox wine-staging" --bootloader grub2 --tmpfs true --tmpfs-options size=16G --apt-http-proxy "http://127.0.0.1:3142" --cache-packages false --gfxoverlays false -a i386 --initsystem systemd
+echo Kanotix slowfire-nightly Slowfire32 $d$v KDE > config/chroot_local-includes/etc/kanotix-version
+lb build; cd tmpfs; ./isohybrid-acritox kanotix32.iso
+check_iso
+mv kanotix32.iso $target/kanotix32-slowfire-nightly-${d}${v}-KDE.iso; cd ..
+cp tmpfs/binary.packages $target/kanotix32-slowfire-nightly-${d}${v}-KDE.packages
+cp tmpfs/binary.log $target/kanotix32-slowfire-nightly-${d}${v}-KDE.log
+cd $target
+if [ ! -f kanotix32-slowfire-nightly-${d}${v}-KDE.iso ]; then
+        echo "$KDISTRO i386 KDE failed" >>isos-failed.txt
+        cd -
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-KDE.log $next/kanotix32-slowfire-nightly-KDE.log
+else
+        md5sum -b kanotix32-slowfire-nightly-${d}${v}-KDE.iso > kanotix32-slowfire-nightly-${d}${v}-KDE.iso.md5
+        zsyncmake kanotix32-slowfire-nightly-${d}${v}-KDE.iso
+        cd -
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-KDE.packages $next/kanotix32-slowfire-nightly-KDE.packages
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-KDE.log $next/kanotix32-slowfire-nightly-KDE.log
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-KDE.iso $next/kanotix32-slowfire-nightly-KDE.iso
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-KDE.iso.zsync $next/kanotix32-slowfire-nightly-KDE.iso.zsync
+        sed "s/${d}${v}-//" $target/kanotix32-slowfire-nightly-${d}${v}-KDE.iso.md5 > $next/kanotix32-slowfire-nightly-KDE.iso.md5
+fi
+
+# lxde 32
+lb clean
+lb config -d $DISTRO -p "kanotix-lxde-master firefox wine-staging" --bootloader grub2 --tmpfs true --tmpfs-options size=16G --apt-http-proxy "http://127.0.0.1:3142" --cache-packages false --gfxoverlays false -a i386 --initsystem systemd
+echo Kanotix slowfire-nightly Slowfire32 $d$v LXDE > config/chroot_local-includes/etc/kanotix-version
+lb build; cd tmpfs; ./isohybrid-acritox kanotix32.iso
+check_iso
+mv kanotix32.iso $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.iso; cd ..
+cp tmpfs/binary.packages $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.packages
+cp tmpfs/binary.log $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.log
+cd $target
+if [ ! -f kanotix32-slowfire-nightly-${d}${v}-LXDE.iso ]; then
+        echo "$KDISTRO i386 LXDE failed" >>isos-failed.txt
+        cd -
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.log $next/kanotix32-slowfire-nightly-LXDE.log
+else
+        md5sum -b kanotix32-slowfire-nightly-${d}${v}-LXDE.iso > kanotix32-slowfire-nightly-${d}${v}-LXDE.iso.md5
+        zsyncmake kanotix32-slowfire-nightly-${d}${v}-LXDE.iso
+        cd -
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.packages $next/kanotix32-slowfire-nightly-LXDE.packages
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.log $next/kanotix32-slowfire-nightly-LXDE.log
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.iso $next/kanotix32-slowfire-nightly-LXDE.iso
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.iso.zsync $next/kanotix32-slowfire-nightly-LXDE.iso.zsync
+        sed "s/${d}${v}-//" $target/kanotix32-slowfire-nightly-${d}${v}-LXDE.iso.md5 > $next/kanotix32-slowfire-nightly-LXDE.iso.md5
+fi
+
+# eeepc4G with LXDE
+lb clean
+lb config -d $DISTRO -p "kanotix-eeepc4G netsurf-gtk" --bootloader grub2 --tmpfs true --tmpfs-options size=16G --apt-http-proxy "http://127.0.0.1:3142" --cache-packages false --gfxoverlays false -a i386 --initsystem systemd
+echo Kanotix slowfire-nightly Slowfire32 $d$v eeepc4G > config/chroot_local-includes/etc/kanotix-version
+lb build; cd tmpfs; ./isohybrid-acritox kanotix32.iso
+check_iso
+mv kanotix32.iso $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso; cd ..
+cp tmpfs/binary.packages $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.packages
+cp tmpfs/binary.log $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.log
+cd $target
+if [ ! -f kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso ]; then
+        echo "$KDISTRO i386 eeepc4G failed" >>isos-failed.txt
+        cd -
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.log $next/kanotix32-slowfire-nightly-eeepc4G.log
+else
+        md5sum -b kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso > kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso.md5
+        zsyncmake kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso
+        cd -
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.packages $next/kanotix32-slowfire-nightly-eeepc4G.packages
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.log $next/kanotix32-slowfire-nightly-eeepc4G.log
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso $next/kanotix32-slowfire-nightly-eeepc4G.iso
+        ln -s $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso.zsync $next/kanotix32-slowfire-nightly-eeepc4G.iso.zsync
+        sed "s/${d}${v}-//" $target/kanotix32-slowfire-nightly-${d}${v}-eeepc4G.iso.md5 > $next/kanotix32-slowfire-nightly-eeepc4G.iso.md5
+fi
+
+fi # end of bookworm build
 
 ################### SPEEDFIRE  ###################
 
